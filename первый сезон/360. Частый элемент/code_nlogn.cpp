d468f42b54d41427d1f3c6844dc0635dfcf1dad3
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-04-14 11:33:37 +0300

    Update code_nlogn.cpp

--- первый сезон/360. Частый элемент/code_nlogn.cpp
+++ первый сезон/360. Частый элемент/code_nlogn.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<algorithm>
 
+using namespace std;
+
+int main() {
+
+	int n; cin >> n;
+
+	map<int, int> foo;
+	for (; n != 0; --n) {
+
+		int a; cin >> a;
+		++foo[a];
+	}
+
+	auto MAX = max_element(foo.rbegin(), foo.rend(), [](const auto& f, const auto& s) {
+		return f.second < s.second;
+		}
+	);
+		 cout << MAX->first;
+
+}
