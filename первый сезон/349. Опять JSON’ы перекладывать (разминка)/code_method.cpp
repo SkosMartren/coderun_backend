cad251a6a56ca2ff194fd4d75f273b4b6fc07ec3
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-04-14 10:14:03 +0300

    Update code_method.cpp

--- первый сезон/349. Опять JSON’ы перекладывать (разминка)/code_method.cpp
+++ первый сезон/349. Опять JSON’ы перекладывать (разминка)/code_method.cpp
@@ -1,1 +1,28 @@
+#include"json.hpp"
+#include<iostream>
+#include<string>
 
+using namespace std;
+using namespace nlohmann;
+
+int main() {
+
+	int n, m; cin >> n >> m;
+	string garbage; getline(cin, garbage);
+
+	json ans;
+	for (int i = 0; i < n; ++i) {
+		if (m == 0) { break; }
+
+		string str_JSON; getline(cin, str_JSON);
+		json tmp =  tmp.parse(str_JSON); 
+
+		for (int j = 0; j < tmp["offers"].size(); ++j) {
+			ans["offers"].push_back(tmp["offers"][j]);
+			--m;
+			if (m == 0) { break; }
+		}
+	}
+
+	cout << ans;
+}
